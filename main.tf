e318e6e978c33a6b67b349a80f6bb9e3db67d58e
Tymofii Orlov <torlo@softserveinc.com>
2019-10-21 16:41:36 +0300

    init commit

---
+++ main.tf
@@ -0,0 +1,90 @@
+#Terraform template for ELK cluster infrastructure
+#Main file
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+module "vpc" {
+  source = "./network"
+}
+
+resource "aws_instance" "torlo-tform-ansible" {
+  count                       = 1
+  ami                         = "ami-088f410fb8317eb48"
+  instance_type               = "t2.micro"
+  subnet_id                   = "${element(module.vpc.aws_pub_subnet, count.index)}"
+  associate_public_ip_address = true
+  vpc_security_group_ids      = ["${module.vpc.sg_pub-ssh}", "${module.vpc.sg_priv-ssh}"]
+  key_name                    = "torlov.test"
+
+  tags = {
+    Name = "torlo-tform-ansible-master"
+  }
+}
+
+resource "aws_instance" "torlo-tform-kibana" {
+  count                       = 1
+  ami                         = "ami-0b432b9079015a8c0"
+  instance_type               = "t2.micro"
+  subnet_id                   = "${element(module.vpc.aws_pub_subnet, count.index)}"
+  associate_public_ip_address = true
+  vpc_security_group_ids      = ["${module.vpc.sg_pub-ssh}", "${module.vpc.sg_priv-ssh}", "${module.vpc.sg_pub-http}"]
+  key_name                    = "torlov.test"
+
+  tags = {
+    Name = "torlo-tform-kibana"
+  }
+}
+resource "aws_instance" "torlo-tform-elastic1" {
+  count                       = 1
+  ami                         = "ami-0b432b9079015a8c0"
+  instance_type               = "t2.micro"
+  subnet_id                   = "${element(module.vpc.aws_pub_subnet, count.index)}"
+  associate_public_ip_address = false
+  vpc_security_group_ids      = ["${module.vpc.sg_priv-elastic}", "${module.vpc.sg_priv-ssh}"]
+  key_name                    = "torlov.test"
+
+  tags = {
+    Name = "torlo-tform-elastic1"
+  }
+}
+resource "aws_instance" "torlo-tform-elastic2" {
+  count                       = 1
+  ami                         = "ami-0b432b9079015a8c0"
+  instance_type               = "t2.micro"
+  subnet_id                   = "${element(module.vpc.aws_pub_subnet, count.index)}"
+  associate_public_ip_address = false
+  vpc_security_group_ids      = ["${module.vpc.sg_priv-elastic}", "${module.vpc.sg_priv-ssh}"]
+  key_name                    = "torlov.test"
+
+  tags = {
+    Name = "torlo-tform-elastic2"
+  }
+}
+resource "aws_instance" "torlo-tform-elastic3" {
+  count                       = 1
+  ami                         = "ami-0b432b9079015a8c0"
+  instance_type               = "t2.micro"
+  subnet_id                   = "${element(module.vpc.aws_pub_subnet, count.index)}"
+  associate_public_ip_address = false
+  vpc_security_group_ids      = ["${module.vpc.sg_priv-elastic}", "${module.vpc.sg_priv-ssh}"]
+  key_name                    = "torlov.test"
+
+  tags = {
+    Name = "torlo-tform-elastic3"
+  }
+}
+resource "aws_instance" "torlo-tform-logstash" {
+  count                       = 1
+  ami                         = "ami-0b432b9079015a8c0"
+  instance_type               = "t2.small"
+  subnet_id                   = "${element(module.vpc.aws_pub_subnet, count.index)}"
+  associate_public_ip_address = false
+  vpc_security_group_ids      = ["${module.vpc.sg_pri-logstash}", "${module.vpc.sg_priv-ssh}"]
+  key_name                    = "torlov.test"
+
+  tags = {
+    Name = "torlo-tform-logstash"
+  }
+}
