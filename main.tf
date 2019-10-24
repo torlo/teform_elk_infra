7a4e71711939be01c591605215157eeae026d58f
torlo <torlo@softserverinc.com>
2019-10-24 15:12:29 +0300

    reconfigure template v0.2

--- main.tf
+++ main.tf
@@ -8,35 +8,164 @@ provider "aws" {
 module "vpc" {
   source = "./modules/network"
 }
-module "srv" {
-  source = "./modules/srv"
+
+resource "aws_instance" "torlo-tform-ansible" {
+  depends_on                  = ["module.vpc"]
+  count                       = 1
+  ami                         = "ami-0f8023ddb0a6acc7a"
+  instance_type               = "t2.micro"
+  subnet_id                   = "${element(module.vpc.aws_pub_subnet, count.index)}"
+  associate_public_ip_address = true
+  vpc_security_group_ids      = ["${module.vpc.sg_pub-ssh}", "${module.vpc.sg_priv-ssh}"]
+  key_name                    = "torlov.test"
+  user_data                   = <<-EOF
+    #!/bin/bash
+    hostnamectl set-hostname ansible
+  EOF
+  tags = {
+    Name = "torlo-tform-ansible-master"
+  }
 }
 
+resource "aws_instance" "torlo-tform-kibana" {
+  depends_on                  = ["module.vpc", "aws_instance.torlo-tform-ansible"]
+  count                       = 1
+  ami                         = "ami-0b432b9079015a8c0"
+  instance_type               = "t2.micro"
+  subnet_id                   = "${element(module.vpc.aws_pub_subnet, count.index)}"
+  associate_public_ip_address = true
+  vpc_security_group_ids      = ["${module.vpc.sg_pub-ssh}", "${module.vpc.sg_priv-ssh}", "${module.vpc.sg_pub-http}"]
+  key_name                    = "torlov.test"
+  user_data                   = <<-EOF
+    #!/bin/bash
+    hostnamectl set-hostname kibana
+  EOF
+  tags = {
+    Name = "torlo-tform-kibana"
+  }
+}
+resource "aws_instance" "torlo-tform-elastic1" {
+  depends_on                  = ["module.vpc", "aws_instance.torlo-tform-ansible"]
+  count                       = 1
+  ami                         = "ami-0b432b9079015a8c0"
+  instance_type               = "t2.micro"
+  subnet_id                   = "${element(module.vpc.aws_pub_subnet, count.index)}"
+  associate_public_ip_address = false
+  vpc_security_group_ids      = ["${module.vpc.sg_priv-elastic}", "${module.vpc.sg_priv-ssh}"]
+  key_name                    = "torlov.test"
+  user_data                   = <<-EOF
+    #!/bin/bash
+    hostnamectl set-hostname elastic1
+  EOF
 
+  tags = {
+    Name = "torlo-tform-elastic1"
+  }
+}
+resource "aws_instance" "torlo-tform-elastic2" {
+  depends_on                  = ["module.vpc", "aws_instance.torlo-tform-ansible"]
+  count                       = 1
+  ami                         = "ami-0b432b9079015a8c0"
+  instance_type               = "t2.micro"
+  subnet_id                   = "${element(module.vpc.aws_pub_subnet, count.index)}"
+  associate_public_ip_address = false
+  vpc_security_group_ids      = ["${module.vpc.sg_priv-elastic}", "${module.vpc.sg_priv-ssh}"]
+  key_name                    = "torlov.test"
+  user_data                   = <<-EOF
+    #!/bin/bash
+    hostnamectl set-hostname elastic2
+  EOF
+  tags = {
+    Name = "torlo-tform-elastic2"
+  }
+}
+resource "aws_instance" "torlo-tform-elastic3" {
+  depends_on                  = ["module.vpc", "aws_instance.torlo-tform-ansible"]
+  count                       = 1
+  ami                         = "ami-0b432b9079015a8c0"
+  instance_type               = "t2.micro"
+  subnet_id                   = "${element(module.vpc.aws_pub_subnet, count.index)}"
+  associate_public_ip_address = false
+  vpc_security_group_ids      = ["${module.vpc.sg_priv-elastic}", "${module.vpc.sg_priv-ssh}"]
+  key_name                    = "torlov.test"
+  user_data                   = <<-EOF
+    #!/bin/bash
+    hostnamectl set-hostname elastic3
+  EOF
+  tags = {
+    Name = "torlo-tform-elastic3"
+  }
+}
+resource "aws_instance" "torlo-tform-logstash" {
+  depends_on                  = ["module.vpc", "aws_instance.torlo-tform-ansible"]
+  count                       = 1
+  ami                         = "ami-0b432b9079015a8c0"
+  instance_type               = "t2.small"
+  subnet_id                   = "${element(module.vpc.aws_pub_subnet, count.index)}"
+  associate_public_ip_address = false
+  vpc_security_group_ids      = ["${module.vpc.sg_pri-logstash}", "${module.vpc.sg_priv-ssh}"]
+  key_name                    = "torlov.test"
+  user_data                   = <<-EOF
+    #!/bin/bash
+    hostnamectl set-hostname logstash
+  EOF
+  tags = {
+    Name = "torlo-tform-logstash"
+  }
+}
+
+output "es1_pri-ip" {
+  value = "${aws_instance.torlo-tform-elastic1.*.private_ip}"
+}
+output "es2_pri-ip" {
+  value = "${aws_instance.torlo-tform-elastic2.*.private_ip}"
+}
+output "es3_pri-ip" {
+  value = "${aws_instance.torlo-tform-elastic3.*.private_ip}"
+}
+output "kibana_pri-ip" {
+  value = "${aws_instance.torlo-tform-kibana.*.private_ip}"
+}
+output "logstash_pri-ip" {
+  value = "${aws_instance.torlo-tform-logstash.*.private_ip}"
+}
+output "ansible-pub-ip" {
+  value = "${aws_instance.torlo-tform-ansible.*.public_ip}"
+}
 
 resource "null_resource" "replace_file_hosts" {
-  #  depends_on = ["template_file.hosts"]
-  count = 1
-  #  depends_on = ["aws_instance.torlo-tform-ansible"]
+  count      = 1
+  depends_on = ["aws_instance.torlo-tform-logstash"]
   connection {
-    host        = "${module.srv.ansible-pub-ip}"
+    host        = "${aws_instance.torlo-tform-ansible[count.index].public_ip}"
     type        = "ssh"
     user        = "ec2-user"
     private_key = "${file("./modules/network/.ssh/id_rsa")}"
     agent       = "false"
   }
-
   provisioner "remote-exec" {
-    inline = ["sudo rm -rf /etc/hosts"]
-  }
-  provisioner "file" {
-    source      = "hosts.tpl"
-    destination = "/home/ec2-user/hosts"
+    inline = ["sudo echo '127.0.0.1   localhost' > /tmp/hosts"]
   }
   provisioner "remote-exec" {
-    inline = ["sudo cp /home/ec2-user/hosts /etc/hosts"]
+    inline = ["sudo echo '${aws_instance.torlo-tform-elastic1[count.index].private_ip}   elastic1.local' >> /tmp/hosts"]
+  }
+  provisioner "remote-exec" {
+    inline = ["sudo echo '${aws_instance.torlo-tform-elastic2[count.index].private_ip}   elastic2.local' >> /tmp/hosts"]
+  }
+  provisioner "remote-exec" {
+    inline = ["sudo echo '${aws_instance.torlo-tform-elastic3[count.index].private_ip}   elastic3.local' >> /tmp/hosts"]
+  }
+  provisioner "remote-exec" {
+    inline = ["sudo echo '${aws_instance.torlo-tform-kibana[count.index].private_ip}   kibana.local' >> /tmp/hosts"]
+  }
+  provisioner "remote-exec" {
+    inline = ["sudo echo '${aws_instance.torlo-tform-logstash[count.index].private_ip}   logstash.local' >> /tmp/hosts"]
+  }
+  provisioner "remote-exec" {
+    inline = ["sudo cp -f /tmp/hosts /etc/hosts"]
   }
   provisioner "remote-exec" {
     inline = ["sudo chown root:root /etc/hosts"]
   }
+
 }
--- templates.tf
+++ templates.tf
@@ -1,10 +1,10 @@
-data "template_file" "hosts" {
-  template = "${file("./hosts.tpl")}"
-  vars = {
-    elastic-ip-1 = "${module.srv.es1_pri-ip}"
-    elastic-ip-2 = "${module.srv.es2_pri-ip}"
-    elastic-ip-3 = "${module.srv.es3_pri-ip}"
-    logstash-ip  = "${module.srv.logstash_pri-ip}"
-    kibana-ip    = "${module.srv.kibana_pri-ip}"
-  }
-}
+#data "template_file" "hosts" {
+#  template = "${file("${path.module}/hosts.tpl")}"
+#  vars = {
+#    elastic-ip-1 = "${module.srv.es1_pri-ip}"
+#    elastic-ip-2 = "${module.srv.es2_pri-ip}"
+#    elastic-ip-3 = "${module.srv.es3_pri-ip}"
+#    logstash-ip  = "${module.srv.logstash_pri-ip}"
+#    kibana-ip    = "${module.srv.kibana_pri-ip}"
+#  }
+#}
